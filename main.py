ff363daebea5e86a8369a48966603a99c06219fe
anikethprasad-toast <aniketh.prasad@toasttab.com>
2022-12-01 00:08:18 -0800

    changes

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from starter import *
 import random
+from collections import deque
+
+def updated_cost(new_node, new_team):
+    pass
+def getSortedEdgeWeightAverage(G) -> dict:
+    node_edge_weight_sums = {}
+    for i in range(G.number_of_nodes()):
+        node_edge_weight_sums[i] = sum(e[1] for e in G.edges(i))
+    s = sorted(node_edge_weight_sums, key=lambda x:node_edge_weight_sums[x])
+    print([node_edge_weight_sums[n] for n in s])
+    return s
+
 
 def solve(G: nx.Graph):
     # TODO implement this function with your solver
     # Assign a team to v with G.nodes[v]['team'] = team_id
     # Access the team of v with team_id = G.nodes[v]['team']
+    sorted_nodes = getSortedEdgeWeightAverage(G)
+    print(sorted_nodes)
+
     for i in range(G.number_of_nodes()):
         G.nodes[i]['team'] = random.randint(1,8)
     
@@ -16,11 +31,11 @@ validate_output(G)
 visualize(G)
 score(G)
 
-#Test accuracy
-run(solve, './inputs/small1.in', 'small1.out', overwrite=True)
+# #Test accuracy
+# run(solve, './inputs/small1.in', 'small1.out', overwrite=True)
 
-#Run all cases
-# run_all(solve, 'input', 'output')
+# #Run all cases
+# run_all(solve, './inputs', 'output')
 # tar('output')
 
    
@@ -28,7 +43,7 @@ run(solve, './inputs/small1.in', 'small1.out', overwrite=True)
 Ideas:
 
     Algorithm:
-    1. Preprocess (sort by # of neighbors, edge weight) + Add node by node with greedy approach, assign current node to best possible team. O(V^2)
+    1. Preprocess (sort by # of neighbors, edge weight) + Add node by node with greedy approach, assign current node to best possible team.
     2. Generate team assignments (randomly or w/greedy?), move nodes until satisfied.
     3. f(nodes) nodes = list of nodes        min_cost(for i in range(len(nodes)): f(nodes \ {i}))
     4. divide n concor?
